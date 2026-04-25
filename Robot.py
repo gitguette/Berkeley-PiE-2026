8e79a8d10e54bca1f50db2119e65776ac0de1fe4
Edward Smith <ejosephsmith2009@gmail.com>
2026-04-25 10:21:56 -0700

    Add support for arm & claw, change driving mechanism

--- Robot.py
+++ Robot.py
@@ -1,39 +1,73 @@
 # Device IDs
-MTR_R = "6_10919479101575700370"
-MTR_L = "6_7490656656070236256"
+m_f = "6_10919479101575700370"
+m_b = "6_7490656656070236256"
+
+arm = "6_13086570018056326489"
+claw = "4_3428342422435623147"
+
+line = "Placeholder"
 
 # Motors
-MTR_F = "a"
-MTR_B = "b"
+fl_wheel = "velocity_b"
+fr_wheel = "velocity_a"
+bl_wheel = "velocity_a"
+br_wheel = "velocity_b"
 
-# Placeholder autonomous code, drive in a loop
-def autonomous():
+def drive(x, y, r, s):
+    motors = [(x + y + r), (x - y - r), (x - y + r), (x + y - r)]
+    normal = abs(max(motors, key=abs))
+    if normal > 1:
+        motors = [i / normal for i in motors]
+    motors = [i * s for i in motors]
+    Robot.set_value(m_f, fl_wheel, -1.0 * motors[0])
+    Robot.set_value(m_f, fr_wheel, -1.0 * motors[1])
+    Robot.set_value(m_b, bl_wheel, motors[2])
+    Robot.set_value(m_b, br_wheel, motors[3])
+
+def move_arm(speed):
+    Robot.set_value(arm, "pid_enabled_b", False)
+    Robot.set_value(arm, "deadband_b", 0.0)
+    # Needed to hold the arm up
+    base = -0.1
+    
+    Robot.set_value(arm, "velocity_b", speed + base)
+
+def calibrate_claw():
     while True:
-        apply_z_velocity(1.0)
-        apply_x_velocity(1.0)
-        Robot.sleep(0.5)
-        apply_z_velocity(-1.0)
-        apply_x_velocity(1.0)
-        Robot.sleep(0.5)
-        apply_z_velocity(-1.0)
-        apply_x_velocity(-1.0)
-        Robot.sleep(0.5)
-        apply_z_velocity(1.0)
-        apply_x_velocity(-1.0)
-        Robot.sleep(0.5)
+        Robot.set_value(claw, "servo1", 90.0)
+        Robot.sleep(2)
+        Robot.set_value(claw, "servo1", 0.0)
+        Robot.sleep(2)
 
-# 1.0: full forward, -1.0: full backward
-def apply_z_velocity(value):
-    Robot.set_value(MTR_L, "velocity_" + MTR_F, value * 1.0)
-    Robot.set_value(MTR_R, "velocity_" + MTR_B, value * -1.0)
+def calibrate():
+    Robot.set_value(m_f, fl_wheel, 1)
+    Robot.sleep(3)
+    Robot.set_value(m_f, fr_wheel, 1)
+    Robot.sleep(3)
+    Robot.set_value(m_b, bl_wheel, 1)
+    Robot.sleep(3)
+    Robot.set_value(m_b, br_wheel, 1)
+    Robot.sleep(3)
 
+"""
+def linefollowmode():
+    while Robot.get_value(line, "center") >= 0.6:
+        drive(0, 1, 0, 1)
+    while Robot.get_value(line, "center") < 0.6:
+"""
 
-# 1.0: full right, -1.0: full left
-def apply_x_velocity(value):
-    Robot.set_value(MTR_L, "velocity_" + MTR_B, value * 1.0)
-    Robot.set_value(MTR_R, "velocity_" + MTR_F, value * -1.0)
+def autonomous():
+    calibrate2()
 
 def teleop():
     while True:
-        apply_z_velocity(Gamepad.get_value("joystick_left_y"))
-        apply_x_velocity(Gamepad.get_value("joystick_left_x"))
+        servo = 0.0
+        rotation = Gamepad.get_value("r_trigger") - Gamepad.get_value("l_trigger")
+        #move_arm(Gamepad.get_value("joystick_right_y") * 0.24)
+        if Gamepad.get_value("button_a") and servo > 0:
+            servo -= 1
+        if not Gamepad.get_value("button_a") and servo < 180:
+            servo += 1
+        Robot.set_value(claw, "servo1", servo)
+        #drive(Gamepad.get_value("joystick_left_x"), Gamepad.get_value("joystick_left_y") * -1.0, rotation, 1.0)
+        drive(Gamepad.get_value("joystick_right_x") * 0.6, Gamepad.get_value("joystick_left_y") * -1.0, 0, 1.0)
