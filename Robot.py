df5514afb41eed84ea6bb3de8defdaf981ba5923
Edward Smith <ejosephsmith2009@gmail.com>
2026-04-25 23:00:19 -0700

    PiE Wars: Return of the Mecanum Wheel. Everything else is broken, but that's ok

--- Robot.py
+++ Robot.py
@@ -5,7 +5,7 @@ m_b = "6_7490656656070236256"
 arm = "6_13086570018056326489"
 claw = "4_3428342422435623147"
 
-line = "Placeholder"
+line = "2_10879054519292937822"
 
 # Motors
 fl_wheel = "velocity_b"
@@ -14,13 +14,15 @@ bl_wheel = "velocity_a"
 br_wheel = "velocity_b"
 
 def drive(x, y, r, s):
-    motors = [(x + y + r), (x - y - r), (x - y + r), (x + y - r)]
+    Robot.set_value(m_f, "invert_b", True)
+    Robot.set_value(m_b, "invert_a", True)
+    motors = [(y + x + r), (y - x - r), (y - x + r), (y + x - r)]
     normal = abs(max(motors, key=abs))
     if normal > 1:
         motors = [i / normal for i in motors]
     motors = [i * s for i in motors]
-    Robot.set_value(m_f, fl_wheel, -1.0 * motors[0])
-    Robot.set_value(m_f, fr_wheel, -1.0 * motors[1])
+    Robot.set_value(m_f, fl_wheel, motors[0])
+    Robot.set_value(m_f, fr_wheel, motors[1])
     Robot.set_value(m_b, bl_wheel, motors[2])
     Robot.set_value(m_b, br_wheel, motors[3])
 
@@ -28,7 +30,7 @@ def move_arm(speed, powermode):
     Robot.set_value(arm, "pid_enabled_a", False)
     Robot.set_value(arm, "deadband_a", 0.0)
     # Needed to hold the arm up
-    base = -0.0002
+    base = -0.0001
     
     if powermode:
         base *= 3
@@ -36,36 +38,32 @@ def move_arm(speed, powermode):
     
     Robot.set_value(arm, "velocity_b", speed + base)
 
-
-def linefollowmode():
-    while True:
-        while Robot.get_value(line, "center") >= 0.6:
-            drive(0, 1, 0, 1)
-        while Robot.get_value(line, "center") < 0.6:
-            if Robot.get_value(line, "left") < 0.3 and Robot.get_value(line, "right") < 0.3:
-                while True:
-                    drive(0, 0, 0, 0)
-            if Robot.get_value(line, "left") > Robot.get_value(line, "right"):
-                drive(0.3, 0, 0, 1)
-            else:
-                drive(-0.3, 0, 0, 1)
-
+def calibrate():
+    Robot.set_value(m_f, fl_wheel, 1.0)
+    Robot.sleep(5)
+    Robot.set_value(m_f, fr_wheel, 1.0)
+    Robot.sleep(5)
+    Robot.set_value(m_b, bl_wheel, 1.0)
+    Robot.sleep(5)
+    Robot.set_value(m_b, br_wheel, 1.0)
+    Robot.sleep(5)
 
 def autonomous():
-    linefollowmode()
+    calibrate()
+#    linefollowmode()
 
 def teleop():
     servo = -1.0
     while True:
+        rotation = 0.3 * (Gamepad.get_value("r_trigger") - Gamepad.get_value("l_trigger"))
+        powermode = False
         if Gamepad.get_value("r_bumper"):
             powermode = True
-        else:
-            arm_mult = False
         servo = 0.0
-        move_arm(Gamepad.get_value("joystick_right_y"), powermode)
-        if Gamepad.get_value("r_trigger"):
-            servo = 0
-        else:
-            servo = 1
-        Robot.set_value(claw, "servo1", servo)
-        drive(Gamepad.get_value("joystick_right_x") * 0.6, Gamepad.get_value("joystick_left_y") * -1.0, 0, 1.0)
+        #move_arm(Gamepad.get_value("joystick_right_y") * 0.1, powermode)
+        #if Gamepad.get_value("r_trigger"):
+        #    servo = 0
+        #else:
+        #    servo = 1
+        #Robot.set_value(claw, "servo1", servo)
+        drive(Gamepad.get_value("joystick_left_x") * 0.6, Gamepad.get_value("joystick_left_y") * -1.0, rotation, 1.0)
